40e475c1e60d24ab3d559a704f60d9025de12a0c
Eric Liang <jklelxy@gmail.com>
2018-05-16 19:01:46 -0500

    added auth

--- server.js
+++ server.js
@@ -1,18 +1,54 @@
-var express        = require("express");
-var exphbs         = require("express-handlebars");
-var path           = require("path");
-var bodyParser     = require("body-parser");
-var app  = express();
-var PORT = process.env.PORT || 3000;
+let express = require("express");
+let exphbs = require("express-handlebars");
+let bodyParser = require("body-parser");
+let passport = require('passport')
+let session = require('express-session')
+let env = require('dotenv').load();
+let PORT = process.env.PORT || 3000;
+
+let app = express();
 
 // Set up Express to handle parsing data
+app.use(bodyParser.urlencoded({
+    "extended": false
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({"extended": false}));
+
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Set handlebars
-app.engine("handlebars", exphbs({"defaultLayout": "main"}));
-app.set("view engine", "handlebars");
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
+app.get('/', function (req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+
+//Models
+let models = require("./app/models");
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function () {
+    console.log('Nice! Database looks fine')
+}).catch(function (err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}.`));
