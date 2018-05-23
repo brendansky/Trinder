c2eae728c3e008a24f2c520e7cabe6554306416d
brendansky <33739060+brendansky@users.noreply.github.com>
2018-05-23 15:25:51 -0500

    Brendansky (#1)
    * added auth
    
    * incorporated front-end files
    
    * added profile pic
    
    * css and js slightly working
    
    * improved user interface
    
    * added item model
    
    * added inventory routes
    
    * minor css upgrades db not working
    
    * db still not working
    
    * created trade div

---
+++ app/public/assets/javascript/items.js
@@ -0,0 +1,27 @@
+var items = [
+
+    computer = {
+        image: "computer.jpg",
+        description: "a computer"
+    },
+
+    guitar = {
+        image: "guitar.jpeg",
+        description: "a guitar"
+    },
+
+    headphones = {
+        image: "headphones.jpg",
+        description: "headphones.jpg"
+    },
+
+    painting = {
+        image: "painting.jpg",
+        description: "a painting"
+    },
+
+    phone = {
+        image: "phone.jpeg",
+        description: "a phone"
+    },
+];
